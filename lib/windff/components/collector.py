34d6f1868c5e1814efd7f396204e0b519a35a4c4
Yiliang Wan <yiliangw@hotmail.com>
2024-10-28 15:16:40 +0800

    lib/windff: update collector

--- lib/windff/components/collector.py
+++ lib/windff/components/collector.py
@@ -1,64 +1,49 @@
-from ..data.raw import TurbRawData, RawDataParsingError
-from dataclasses import dataclass
 from flask import Flask, request, jsonify
+
 import logging
 
-import influxdb_client as influxc
-from influxdb_client.client.write_api import SYNCHRONOUS
+from ..env import Env
+from .component import Component
+from ..data.raw import RawTurbData
+from ..errors import WindffDBError, WindffRawDataParsingError
 
 
-class Collector(object):
+class Collector(Component):
 
-  @dataclass
-  class Config:
-    turb_raw_dtype: type
-    listen_addr: str
-    listen_port: int
-    raw_db_url: str
-    raw_db_toekn: str
-    raw_db_org: str
-    turb_bucket: str
+  def get_type(cls):
+    return Component.Type.COLLECTOR
 
-  def __init__(self, config: Config):
-    self.config = config
+  def __init__(self):
+    self.env: Env = None
 
     self.server: Flask = None
 
-    self.influxdb_client = None
-    self.raw_db_write_api = None
+  def initialize(self, env):
+    logging.info("Initializing collector...")
+    self.env = env
+    logging.info("Collector initialized.")
 
-  def __receive_turb_raw_data(self, data: TurbRawData):
+  def __receive_turb_raw_data(self, data: RawTurbData):
     try:
-      self.raw_db_write_api.write(
-          bucket=self.config.turb_bucket, record=data.to_influxdb_point())
-      return True
-    except influxc.client.InfluxDBError as e:
-      logging.error(
-          f"Error writing to InfluxDB: data({str(data)}) err({str(e)})")
-      return False
+      self.env.write_raw_turb_data(data)
+    except WindffDBError as e:
+      logging.warning("Ignored raw turbine data due to DB error.")
 
   def __start_flask_server(self):
     self.server = Flask(__name__)
 
-    @self.server.route("/turb_raw_data", methods=["POST"])
+    @self.server.route("/raw_turb_data", methods=["POST"])
     def handle_turb_raw_data():
       try:
         data = self.config.turb_raw_dtype.from_json(request.data)
         res = self.__receive_turb_raw_data(data)
         if not res:
           return jsonify({"error": "Error writing to InfluxDB"}), 500
-      except RawDataParsingError as e:
+      except WindffRawDataParsingError as e:
         logging.warning(f"Error parsing data: {str(e)}")
         return jsonify({"error": str(e)}), 400
 
     self.server.run(host=self.config.listen_addr, port=self.config.listen_port)
 
-  def __start_influxdb_client(self):
-    self.influxdb_client = influxc.InfluxDBClient(
-        url=self.config.raw_db_url, token=self.config.raw_db_token, org=self.config.raw_db_org)
-    self.raw_db_write_api = self.influxdb_client.write_api(
-        write_options=SYNCHRONOUS)
-
   def start(self):
-    self.__start_influxdb_client()
     self.__start_flask_server()
